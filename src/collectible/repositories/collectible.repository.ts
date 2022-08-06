d5a07840f7a77792eb2d83091bef19b2ddee1e91
Iriabow <guilderwars@gmail.com>
2022-08-06 21:03:50 +0200

    Añadido populate en el repo de timeline para que traiga user y spaces

--- src/collectible/repositories/collectible.repository.ts
+++ src/collectible/repositories/collectible.repository.ts
@@ -32,7 +32,9 @@ export class CollectibleRepository extends AbstractRepository<Collectible>{
                     }
                 }
             ]
-        }, {}, { lean: true });
+        }, {}, { lean: true })
+            .populate("thematicSpace")
+            .populate("user");
     }
 
     /**
