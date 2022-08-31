ad5d6d5d1908682eda2bcc7a5db1929fccca84c0
Juan Carlos Utrilla Martín <juancarlos.utrilla@gmail.com>
2022-08-31 07:55:03 +0200

    Remove unusued elements

--- src/files/dto/update-file-upload.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFileDto as CreateFileDto } from './create-file-upload.dto';
-
-export class UpdateFileDto extends PartialType(CreateFileDto) {}
