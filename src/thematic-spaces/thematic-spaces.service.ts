da431e0e3d7e5ad914464042d87ce0da22cf09ac
Juan Carlos Utrilla Martín <juancarlos.utrilla@gmail.com>
2022-09-02 22:35:11 +0200

    Purge and test

--- src/thematic-spaces/thematic-spaces.service.ts
+++ src/thematic-spaces/thematic-spaces.service.ts
@@ -19,29 +19,7 @@ export class ThematicSpacesService {
     //@InjectModel(ThematicSpace.name) private thematicSpaceModel: Model<ThematicSpaceDocument>,
   ) {}
 
-  async getOwnedThematicSpaces(userId: string){
-    // Paso 1: obtenemos el usuario de la base de datos
-    const user = await this.userService.findOneById(userId);
-
-    // Paso 2: sacamos sus espacios temáticos
-    const thematicSpacesId = user.ownedThematicSpaces.map( thematicSpace => thematicSpace._id );
-
-    // Paso 3: buscamos los espacios temáticos en la base de datos
-    return await this.thematicSpaceRepository.getThematicSpacesByIds(thematicSpacesId);
-  }
-
-  async getFollowedThematicSpaces(userId: string) {
-    // Paso 1: obtenemos el usuario de la base de datos
-    const user = await this.userService.findOneById(userId);
-
-    // Paso 2: sacamos sus espacios temáticos
-    const thematicSpacesId = user.followedThematicSpaces.map( thematicSpace => thematicSpace._id );
-
-    // Paso 3: buscamos los espacios temáticos en la base de datos
-    return await this.thematicSpaceRepository.getThematicSpacesByIds(thematicSpacesId);
-  }
-
-  async followSpaceByUserId(userId: string, thematicSpaceId: string) {
+  async followSpaceByUserId(userId: string, thematicSpaceId: string)   {
     let user: Document<unknown, any, Users> & Users & Required<{ _id: Types.ObjectId; }>
 
     // Comprobación: Espacio temático y Usuario existen
@@ -71,11 +49,7 @@ export class ThematicSpacesService {
   async create(thematicSpace: ThematicSpace) {
     return await this.thematicSpaceRepository.add(thematicSpace);
   }
-
-  async findAll() {
-    return await this.thematicSpaceRepository.find({});
-  }
-
+  
   async findOneById(id: string) {
     return await this.thematicSpaceRepository.findOne({ _id: id });
   }
@@ -86,17 +60,6 @@ export class ThematicSpacesService {
 
   async update(id: string, thematicSpace: ThematicSpace) {
     return await this.thematicSpaceRepository.findOneAndUpdate( { _id: id }, thematicSpace );
-  }
-
-  async remove(id: string) {
-    return await this.thematicSpaceRepository.delete( { _id: id } );
-  }
-
-  async removeByTag(tag: string) {
-    // const query = this.thematicSpaceModel.find({ _id: '62daca441334ad9783770397' }).where()
-    // return this.thematicSpaceRepository.find( { name: { "$regex": "Testing"} });
-    return this.thematicSpaceRepository.removeByTag(tag);
-    // return await this.thematicSpaceRepository.find({});
   }
 
   async populate(){
